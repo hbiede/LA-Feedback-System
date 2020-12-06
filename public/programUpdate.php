baf1626c849d57f70707f0154540114d64d6532f
hbiede <6586509+hbiede@users.noreply.github.com>
2020-12-06 17:50:08 -0600

    Only send applicable sections in updates

--- public/programUpdate.php
+++ public/programUpdate.php
@@ -19,6 +19,8 @@ function should_update($days = DAYS) {
             $count += $row['count'];
         }
         $ps->close();
+    } else {
+        error_log('Failed to build should_update ps');
     }
     $conn->close();
     return $count > 0;
@@ -28,7 +30,8 @@ function get_interaction_counts($days) {
     $conn = get_connection();
     $ps = $conn->prepare("SELECT course, COUNT(*) AS 'count' FROM interactions WHERE time_of_interaction >= " .
         "(CURRENT_DATE() - INTERVAL ? DAY) GROUP BY course;");
-    $return_val = '<h4>Course Interactions:</h4><ul>';
+    $return_val = '';
+    $header = '<h4>Course Interactions:</h4><ul>';
     if ($ps) {
         $ps->bind_param('i', $days);
         $ps->execute();
@@ -37,10 +40,12 @@ function get_interaction_counts($days) {
             $return_val .= "<li>" . $row['course'] . ' - ' . $row['count'] . "</li>";
         }
         $ps->close();
-        $return_val .= '</ul>';
+        if (strlen($return_val) > 0) {
+            $return_val .= '</ul>';
+        }
     }
     $conn->close();
-    return $return_val;
+    return strlen($return_val) > 0 ? ($header . $return_val) : '';
 }
 
 function get_course_averages($days) {
@@ -48,7 +53,8 @@ function get_course_averages($days) {
     $ps = $conn->prepare("SELECT course, AVG(rating) AS 'avg', AVG(sentiment) AS 'sentiment' FROM feedback " .
         "LEFT JOIN interactions i on feedback.interaction_key = i.interaction_key WHERE " .
         "time_of_interaction >= (CURRENT_DATE() - INTERVAL ? DAY) GROUP BY course ORDER BY course;");
-    $return_val = '<h4>Course Feedback Averages:</h4><ul>';
+    $return_val = '';
+    $header = '<h4>Course Feedback Averages:</h4><ul>';
     if ($ps) {
         $ps->bind_param('i', $days);
         $ps->execute();
@@ -58,19 +64,15 @@ function get_course_averages($days) {
                 ' (Avg Comment Sentiment: ' . round($row['sentiment'], 2) . "%)</li>";
         }
         $ps->close();
-        $return_val .= '</ul>';
+        if (strlen($return_val) > 0) {
+            $return_val .= '</ul>';
+        }
     }
     $conn->close();
-    return $return_val;
+    return strlen($return_val) > 0 ? ($header . $return_val) : '';
 }
 
-function get_update_string($days = DAYS) {
-    $course_counts = get_interaction_counts($days);
-    if (strlen($course_counts) === 0) {
-        return '';
-    }
-    $course_averages = get_course_averages($days);
-
+function get_feedback($days = DAYS) {
     $conn = get_connection();
     $ps = $conn->prepare('SELECT DATE_FORMAT(time_of_interaction, \'%Y-%m-%dT%TZ\') AS time, ' .
         'interaction_type, sentiment, IFNULL(name, username) AS LA, rating, ' .
@@ -104,13 +106,18 @@ function get_update_string($days = DAYS) {
         error_log($conn->error);
     }
     $conn->close();
+    if (count($la_feedback) === 0) return '';
 
-    $return_string = $course_counts . $course_averages . "<br><br><h4>Individual LA Feedback:</h4><br>";
+    $return_string = "<br><br><h4>Individual LA Feedback:</h4><br>";
     krsort($la_feedback);
     foreach ($la_feedback as $key => $value) {
         $return_string .= $value . "</ul><br>";
     }
     return $return_string;
+}
+
+function get_update_string($days = DAYS) {
+    return get_interaction_counts($days) . get_course_averages($days) . get_feedback($days);
 }
 
 function send_email($report) {
@@ -126,7 +133,9 @@ function send_email($report) {
     }
 }
 
-$update_string = get_update_string();
-if (should_update() && strlen($update_string) > 0) {
-    send_email($update_string);
+if (should_update()) {
+    $update_string = get_update_string();
+    if (strlen($update_string) > 0) {
+        send_email($update_string);
+    }
 }
