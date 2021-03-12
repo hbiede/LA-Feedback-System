5e6ab12f6991d49ac99ca500ac516ab420b42cad
hbiede <6586509+hbiede@users.noreply.github.com>
2021-03-12 16:00:43 -0600

    Simplify findStudent

--- public/findStudent.php
+++ public/findStudent.php
@@ -2,7 +2,9 @@
 
 // Call with a POST call with a JSON body as follows:
 //{
-//  canvas_username: string
+//  canvas_username: string;
+//  name?: string; (optional name of the student)
+//  email?: string; (optional email of the student)
 //}
 
 // Returns a JSON encoded message as follows:
@@ -13,17 +15,13 @@
 include_once 'sqlManager.php';
 ini_set('error_log', './log/find_student.log');
 
-function get_students($username) {
-    return run_accessor('SELECT `username_key` FROM `cse_usernames` WHERE `canvas_username` = "' . $username .'"');
-}
-
 $obj = json_decode(file_get_contents('php://input'));
-  if (empty($obj->{'canvas_username'})) {
+if (isset($obj->{'canvas_username'})) {
+    echo get_username_id($obj->{'canvas_username'}, $obj->{'name'}, $obj->{'email'});
+} else {
     header('Status: 400');
     echo json_encode([
         'status' => 400,
         'message' => 2
     ]);
-  } else {
-    echo json_encode(get_students($obj->{'canvas_username'}));
-  }
+}
