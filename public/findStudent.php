57cb77b3927bdba3cf7e4e51202235403fbdc7ef
Pengxiang Zhang <z-git@protonmail.com>
2021-03-30 15:54:09 -0500

    Only return error when user not find in DB

--- public/findStudent.php
+++ public/findStudent.php
@@ -18,15 +18,13 @@ ini_set('error_log', './log/find_student.log');
 $obj = json_decode(file_get_contents('php://input'));
 if (isset($obj->{'canvas_username'})) {
     echo json_encode([
-        'status' => 400,
-        'message' => 2,
+        'status' => 200,
         'username_key' => get_username_id($obj->{'canvas_username'}, $obj->{'name'}, $obj->{'email'}, false),
     ]);
 } else {
     header('Status: 400');
     echo json_encode([
         'status' => 400,
-        'message' => 2,
         'username_key' => -1,
     ]);
 }
