862a499849f721884611ead87f323f39e94a1e4c
Pengxiang Zhang <z-git@protonmail.com>
2021-03-30 21:38:22 -0500

    Add response message

--- public/findStudent.php
+++ public/findStudent.php
@@ -9,8 +9,16 @@
 
 // Returns a JSON encoded message as follows:
 //{
+//  status: number (HTML response code),
+//  message?: int
 //  username_key: int
 //}
+
+/*
+ * Response codes:
+ * 0 - Success
+ * 1 - User not find in DB
+ */
 
 include_once 'sqlManager.php';
 ini_set('error_log', './log/find_student.log');
@@ -19,12 +27,14 @@ $obj = json_decode(file_get_contents('php://input'));
 if (isset($obj->{'canvas_username'})) {
     echo json_encode([
         'status' => 200,
+        'message' => 0,
         'username_key' => get_username_id($obj->{'canvas_username'}, $obj->{'name'}, $obj->{'email'}, false),
     ]);
 } else {
     header('Status: 400');
     echo json_encode([
         'status' => 400,
+        'message' => 1,
         'username_key' => -1,
     ]);
 }
