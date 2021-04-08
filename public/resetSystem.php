968ff301a07ccd6760b8e5e3cf812f0919a6dd56
hbiede <6586509+hbiede@users.noreply.github.com>
2021-04-07 19:20:32 -0500

    Improve error logging in resetSystem.php

--- public/resetSystem.php
+++ public/resetSystem.php
@@ -1,14 +1,18 @@
 <?php
 
-ini_set('error_log', './log/resetSystem.log');
-
 if ($argc > 1) {
     $sql_info = json_decode(file_get_contents("data/sql.json"));
 
-    $command = "mysql --user={$sql_info->{'username'}} --password='{$sql_info->{'password'}}' "
-        . "-h {$sql_info->{'url'}} -D {$sql_info->{'database'}} < ./data/tableSetup.sql";
+    if ($sql_info === null) {
+        fwrite(STDERR, "Cannot decode sql login information\n");
+    } else {
+        $command = "mysql --user={$sql_info->{'username'}} --password='{$sql_info->{'password'}}' "
+            . "-h {$sql_info->{'url'}} -D {$sql_info->{'database'}} < ./data/tableSetup.sql";
 
-    $output = shell_exec($command . "\n");
+        if (system($command . "\n") === false) {
+            fwrite(STDERR, "Failed to execute\n");
+        }
+    }
 } else {
-    fwrite(STDERR, "Invalid run\n");
+    fwrite(STDERR, "Invalid arguments\n");
 }
